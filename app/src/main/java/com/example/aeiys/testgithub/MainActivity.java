edd174faa7c9514627b9d4191cc52ebe6ef5a537
KhadijahAl_Saggaf <ksakkaf@stu.kau.edu.sa>
2018-11-01 23:16:58 +0300

    second khadoug test commet

--- app/src/main/java/com/example/aeiys/testgithub/MainActivity.java
+++ app/src/main/java/com/example/aeiys/testgithub/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.aeiys.testgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //second test of commet
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
